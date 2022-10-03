b4172c88acc8f6b5cb94a4f70ed1a1a505e83492
Ilya Tituchenko <titu4enko@mail.ru>
2022-10-03 22:32:30 +0300

    Ex 41 solved Plus number

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 //0, 7, 8, -2, -2 -> 2
 
 //1, -7, 567, 89, 223-> 3
+/*
 int[] arr=GetArrayFromString ();
 printArray(arr);
 Console.WriteLine ($" -> {checkPlus(arr)}");
@@ -56,8 +57,14 @@ void printArray (int[] arr)
 {
     Console.Write ($"[{String.Join(",",arr)}]");
 }
-
+*/
 
 //Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine ("Введите коэффициенты первой прямой y=k1*x+b1.");
+Console.Write ("k1=");
+int k1= int.Parse(Console.ReadLine()!);
+Console.Write ("b1=");
+int b1= int.Parse(Console.ReadLine()!);
