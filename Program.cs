e8bca644c429a0d53ba2880fbb7a957bea2f1fb2
Ilya Tituchenko <titu4enko@mail.ru>
2022-10-03 23:01:05 +0300

    Ex 43 Solved find pixel

--- Program.cs
+++ Program.cs
@@ -65,6 +65,29 @@ void printArray (int[] arr)
 
 Console.WriteLine ("Введите коэффициенты первой прямой y=k1*x+b1.");
 Console.Write ("k1=");
-int k1= int.Parse(Console.ReadLine()!);
+double k1= double.Parse(Console.ReadLine()!);
 Console.Write ("b1=");
-int b1= int.Parse(Console.ReadLine()!);
+double b1= double.Parse(Console.ReadLine()!);
+
+Console.WriteLine ("Введите коэффициенты второй прямой y=k2*x+b2.");
+Console.Write ("k2=");
+double k2= double.Parse(Console.ReadLine()!);
+Console.Write ("b2=");
+double b2= double.Parse(Console.ReadLine()!);
+
+
+
+if (check(k1,k2)) { 
+    double x=(b2-b1)/(k1-k2);
+    double y=(b2*k1-b1*k2)/(k1-k2);
+    Console.WriteLine ($"Точка пересечения ({x};{y})");
+}
+bool check (double k1, double k2) {
+    if (k1==k2)   
+    {
+        Console.WriteLine ("Не пересекаются!");
+        return false;
+    }
+    return true;
+
+}
